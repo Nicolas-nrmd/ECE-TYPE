c8dadbe5e5344a4e4ba72d2216f1b34c6b36fb0b
Audran-kn <audran.chevalier@edu.ece.fr>
2025-05-19 16:52:19 +0200

    Update header.c

--- header.c
+++ header.c
@@ -5,6 +5,7 @@
 #include <allegro.h>
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 #define LARGEUR_ECRAN 800
 #define HAUTEUR_ECRAN 546
@@ -17,7 +18,7 @@
 #define MAX_POISSONS 50
 #define NB_TYPES_POISSONS 4
 #define MAX_BULLES 100
-#define TEMPS_CLIGNOTE 300
+#define TEMPS_CLIGNOTE 100
 #define DELAI_BULLE 100
 
 #define MAX_PSEUDO 20
@@ -26,6 +27,14 @@
 #define MAX_BULLES_ETOILE 20
 #define DELAI_TIR_ETOILE 100
 
+#define MAX_REQUINS 10
+#define DELAI_TIR_REQUIN 100
+#define DEGAT_REQUIN 50
+
+#define DELAI_BULLE_BOSS 60
+#define BOSS_FRAME_COUNT 5
+#define BOSS_FRAME_WIDTH 187
+#define BOSS_FRAME_HEIGHT 159
 
 typedef struct {
     int x, y;
@@ -64,6 +73,58 @@ typedef struct {
     int pris;
 } Bonus;
 
+typedef struct {
+    int x, y;
+    int actif;
+    int pris;
+} BouclierBonus;
+
+typedef struct {
+    int actif;
+    int x, y;
+    int y_init;
+    int hp;
+    BITMAP *spritesheet;
+    int frame;
+    int frame_timer;
+    int largeur_frame;
+    int hauteur_frame;
+    int nb_frames;
+} Boss;
+
+typedef struct {
+    int x, y;
+    int dx, dy;
+    int actif;
+} epine_etoile_t;
+
+typedef struct {
+    int x, y;
+    int actif;
+    int delai_tir;
+    clock_t temps_touche;
+    int clignote;
+} Requin;
+
+#define MAX_DENTS 50
+
+typedef struct {
+    int actif;
+    float x, y;
+    float dx, dy;
+} Dent;
+
+Dent dents[MAX_DENTS];
+
+
+Requin requins[MAX_REQUINS];
+BITMAP *requin_img;
+
+extern BouclierBonus boucliers[3];
+extern BITMAP *bouclier_img;
+extern int bouclier_actif;
+
+
 
 Torpille torpilles[MAX_TORPILLES];
 Poisson poissons[MAX_POISSONS];
@@ -77,25 +138,36 @@ BITMAP *etoile_img;
 BITMAP *poisson_imgs[NB_TYPES_POISSONS];
 BITMAP *coeur_img, *bulle_img;
 
-int est_position_valide(int x, int y, BITMAP *collision, BITMAP *vaisseau);
-int position_poisson_valide(int x, int y, BITMAP *img, BITMAP *collision);
+int positionValide(int x, int y, BITMAP *collision, BITMAP *vaisseau);
+int positionPoissonValide(int x, int y, BITMAP *img, BITMAP *collision);
 
-void tirer_torpille(int x, int y);
-void tirer_bulle(int x, int y);
-void ajouter_banc_poissons(int camera_x, BITMAP *collision);
-void generer_bonus(int camera_x, BITMAP *collision, BITMAP *coeur_img);
+void tireTorpille(int x, int y);
+void tireBulle(int x, int y);
+void Banc_Poisson(int camera_x, BITMAP *collision);
+void BonusLife(int camera_x, BITMAP *collision, BITMAP *coeur_img);
 
-void textout_gros(BITMAP* bmp, FONT* f, const char* txt, int x, int y, int color);
-void sauvegarder_progression(const char* pseudo, int niveau);
-int charger_progression(const char* pseudo_cherche, int* niveau_retourne);
-void dessiner_ecran_menu(const char* titre, const char* options[], int n, int selection);
-int saisir_pseudo_utilisateur(char* destination);
-void menu_niveaux();
+void textegros(BITMAP* bmp, FONT* f, const char* txt, int x, int y, int color);
+void sauvegarde(const char* pseudo, int niveau);
+int chargementPartie(const char* pseudo_cherche, int* niveau_retourne);
+void EcranMenu(const char* titre, const char* options[], int n, int selection);
+int GestionPseudo(char* destination);
+void gestionNiveaux();
 int menu();
-
+void ajouter_etoiles(int camera_x, BITMAP *collision);
+void tirer_bulle_etoile(int x, int y);
 void jouer_niveau1(void);
+void gestionVictoire(int niveau_actuel);
+
+void ajouter_requin(int camera_x, int y_min, int y_max);
+void jouer_niveau2(void);
+void ajouterPoisson(int camera_x, BITMAP *collision);
 
 
+void jouer_niveau3(void);
 
+#define MAX_EPINES_ETOILE 50
+extern epine_etoile_t epines_etoile[MAX_EPINES_ETOILE];
 
+void tirer_epine_etoile(int x, int y);
+epine_etoile_t epines_etoile[MAX_EPINES_ETOILE];
 #endif //HEADER_H
